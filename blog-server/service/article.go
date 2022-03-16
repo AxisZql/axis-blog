3e294e4d92383bd9169fb8d51f8b6f9c5f0d9982
AxisZql <mifei23333@outlook.com>
2022-03-16 23:41:56 +0800

    feat:make the application can run

--- blog-server/service/article.go
+++ blog-server/service/article.go
@@ -2,8 +2,23 @@ package service
 
 import (
 	ctrl "blog-server/controllers"
+	"github.com/gin-gonic/gin"
 )
 
 type Article struct {
 	ctrl.ArticleHandle
 }
+
+func (a *Article) ListArchives(ctx *gin.Context)           {}
+func (a *Article) ListArticles(ctx *gin.Context)           {}
+func (a *Article) ListArticleBack(ctx *gin.Context)        {}
+func (a *Article) SaveOrUpdateArticle(ctx *gin.Context)    {}
+func (a *Article) UpdateArticleTop(ctx *gin.Context)       {}
+func (a *Article) UpdateArticleDelete(ctx *gin.Context)    {}
+func (a *Article) SaveArticleImages(ctx *gin.Context)      {}
+func (a *Article) DeleteArticle(ctx *gin.Context)          {}
+func (a *Article) GetArticleBackById(ctx *gin.Context)     {}
+func (a *Article) GetArticleById(ctx *gin.Context)         {}
+func (a *Article) ListArticleByCondition(ctx *gin.Context) {}
+func (a *Article) ListArticleBySearch(ctx *gin.Context)    {}
+func (a *Article) SaveArticleLike(ctx *gin.Context)        {}
--- blog-server/service/logger.go
+++ blog-server/service/logger.go
@@ -2,8 +2,12 @@ package service
 
 import (
 	ctrl "blog-server/controllers"
+	"github.com/gin-gonic/gin"
 )
 
 type Logger struct {
 	ctrl.LoggerHandler
 }
+
+func (l *Logger) ListOperationLogs(*gin.Context)   {}
+func (l *Logger) DeleteOperationLogs(*gin.Context) {}
